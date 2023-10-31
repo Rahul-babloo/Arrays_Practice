3a34c54fa27668edf17ea8a4f7ba6af2c9cdcdb3
Rahul-babloo <104113247+Rahul-babloo@users.noreply.github.com>
2023-10-31 09:55:46 +0530

    EliminateDups.java

--- EliminateDups.java
+++ EliminateDups.java
@@ -12,25 +12,25 @@ public class EliminateDups {
         int n=sc.nextInt();
         int arr[]=new int[n];
 
-//        for(int i=0;i<arr.length;i++){
-//            arr[i]=sc.nextInt();
-//        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
 
-        //System.out.print("Unique element of array are: ");
+        System.out.print("Unique element of array are: ");
 
-//        for(int i=0;i<arr.length;i++){
-//
-//            boolean isDuplicate=false;
-//            for(int k=i-1;k>=0;k--){
-//                if(arr[i]==arr[k]){
-//                    isDuplicate=true;
-//                    break;
-//                }
-//            }
-//            if(!isDuplicate) {
-//                System.out.print(arr[i]+" ");
-//            }
-//
-//        }
+        for(int i=0;i<arr.length;i++){
+
+            boolean isDuplicate=false;
+            for(int k=i-1;k>=0;k--){
+                if(arr[i]==arr[k]){
+                    isDuplicate=true;
+                    break;
+                }
+            }
+            if(!isDuplicate) {
+                System.out.print(arr[i]+" ");
+            }
+
+        }
     }
 }
